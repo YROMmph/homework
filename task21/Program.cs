9fefa0672c435dd9b1f7177f4dc4464998062ab7
Tolya <tollyatti25503@gmail.com>
2023-06-30 11:37:13 +1000

    added task19, task21, tack23

---
+++ task21/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+int Promt(string message)
+{
+Console.Write(message);
+int distance = int.Parse(Console.ReadLine());
+return distance;
+}
+
+int x1 =  Promt ("Введите координаты точка x A: ");
+int y1 =  Promt ("Введите координаты точка y A: ");
+int z1 =  Promt ("Введите координаты точка z A: ");
+int x2 =  Promt ("Введите координаты точка x B: ");
+int y2 =  Promt ("Введите координаты точка y B: ");
+int z2 =  Promt ("Введите координаты точка z B: ");
+
+double AB = Math.Sqrt((x2 - x1)*(x2 - x1)+(y2 - y1)*(y2 - y1)+(z2 - z1)*(z2 - z1));
+
+Console.WriteLine(Math.Round(AB, 2));
+
---
+++ task23/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+int number = Promt("Введите число: ");
+int i = 1;
+int Promt(string message){
+    Console.Write(message);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+while ( i <= number){
+    Console.WriteLine(i*i*i + " ");
+    i++;
+}
