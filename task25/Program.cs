60c5463ce65a6ef0ebea1e467e8281159acc2a89
Tolya <tollyatti25503@gmail.com>
2023-07-02 16:09:49 +1000

    added task 25, task 27, task 29

---
+++ task25/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int A = ReadInt("Введите число А: ");
+int B = ReadInt("Введите число B: ");
+ToDegree(A,B);
+void ToDegree(int A,int B){
+    int step =1;
+    for (int i = 1; i<=B;i++){
+        step = step * A;
+    }
+    Console.WriteLine(step);
+}
+int ReadInt(string message){
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+
+}
---
+++ task27/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int number = ReadInt("Введите число: ");
+int L = numberlen(number);
+SumNumbers(number, L);
+
+int ReadInt(string message){
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int numberlen(int a){
+    int index = 0;
+    while(a>0){
+        a/=10;
+        index++;
+    }
+    return index;
+}
+
+void SumNumbers(int n, int L){
+    int sum = 0;
+    for (int i = 1; i<=L;i++){
+        sum+= n%10;
+        n/=10;
+
+    }
+    Console.WriteLine($"{sum} - Сумма цифр в числе") ;
+}
+
+    // if (){
+    //     Console.WriteLine();
+    // } else {
+    //     Console.WriteLine();
+    // }
+
