7b10eec16e42ca81b734ab2a18c8c64be4c26dcc
Tolya <tollyatti25503@gmail.com>
2023-07-04 14:38:12 +1000

    added task 41, task 43 and update task38

--- task38/Program.cs
+++ task38/Program.cs
@@ -1,41 +1,67 @@
 ﻿// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
 // [3.22, 4.2, 1.15, 77.15, 65.2] => 77.15 - 1.15 = 76
-int size = 10;
-int[] numbers = new int[size];
-FillArrayRandomNumbers(numbers);
-PrintArray(numbers);
+// int size = 10;
+// int[] numbers = new int[size];
+// FillArrayRandomNumbers(numbers);
+// PrintArray(numbers);
 
-int max = numbers[0];
-int min = numbers[0];
+Console.Clear();
+double[] startArray = GetArray(5);
+Console.WriteLine(String.Join("|", startArray));
+Console.WriteLine($"Разница = {GetDifference(startArray)}");
 
-for (int i = 0; i < numbers.Length; i++)
-{
-    if (numbers[i] > max)
-    {
-        max = numbers[i];
+double[] GetArray(int size){
+    double[] res = new double[size];
+    for (int i = 0; i <size;i++){
+        res[i]= Math.Round(new Random().NextDouble()*20 - 10,3);
+        
     }
-    else if (numbers[i] < min)
-    {
-        min = numbers[i];
-    }
+    return res;
 }
 
-Console.WriteLine($"Минимальное число: {min}");
-Console.WriteLine($"Минимальное число: {max}");
-Console.WriteLine($"Разница между максимальным и минимальным числами: {max-min}");
+double GetDifference(double[] array){
+    double min = array[0];
+    double max = array[0];
+    foreach (double el in array){
+        if (min>el) min = el;
+        if (max<el) max = el;
+    }
+    return max - min;
+}
 
-void FillArrayRandomNumbers(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        array[i] = new Random().Next(1, 555);
-    }
-}
-void PrintArray(int[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
-}
+
+
+// int max = numbers[0];
+// int min = numbers[0];
+
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] > max)
+//     {
+//         max = numbers[i];
+//     }
+//     else if (numbers[i] < min)
+//     {
+//         min = numbers[i];
+//     }
+// }
+
+// Console.WriteLine($"Минимальное число: {min}");
+// Console.WriteLine($"Минимальное число: {max}");
+// Console.WriteLine($"Разница между максимальным и минимальным числами: {max-min}");
+
+// void FillArrayRandomNumbers(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         array[i] = new Random().Next(1, 555);
+//     }
+// }
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.WriteLine();
+// }
---
+++ task43/Program.cs
@@ -0,0 +1,49 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+const int coefficient = 0;
+const int constant = 1;
+const int x_coord = 0;
+const int y_coord = 1;
+const int line1=1;
+const int line2=2;
+
+double[] linedata1 = InputLineData(line1);
+double[] linedata2 = InputLineData(line2);
+
+if (ValidateLines(linedata1,linedata2)){
+    double[] coord = findCoords(linedata1,linedata2);
+    Console.Write($"Точка пересечения уравнений y={linedata1[coefficient]}*x+{linedata1[constant]} и y={linedata2[coefficient]}*x+{linedata2[constant]} ");
+    Console.WriteLine($"имеет координаты ({coord[x_coord]}, {coord[y_coord]})");
+}
+double Prompt(string message){
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    double result = Convert.ToDouble(value);
+    return result;
+}
+double[] InputLinedata(int numberOfLine){
+    double[] linedata = new double[2];
+    linedata[coefficient] = Prompt($"введите коэфициент для {numberOfLine} прямой >");
+    linedata[constant] = Prompt($"Введите константу для {numberOfLine} прямой >");
+    return linedata;
+
+}
+double[] findCoords (double[] linedata1, double[] linedata2){
+    double[] coord = new double[2];
+    coord[x_coord]=(linedata1[constant]- linedata2[constant]) / (linedata2[coefficient]-linedata1[coefficient]);
+    coord[y_coord]=linedata1[constant]* coord[x_coord]+ linedata1[constant];
+    return coord;
+}
+bool ValidateLines(double[] linedata1, double[] linedata2){
+    if (linedata1[coefficient]== linedata2[coefficient]){
+        if (linedata1[constant]== linedata2[constant]){
+            Console.WriteLine("Прямые совпадают");
+            return false;
+        } else{
+            Console.WriteLine("Прямые параллельны");
+            return false;
+        }
+    }
+    return true;
+}
