0967eeeacad5d9b6377760f9fdc6ec393cc00624
Tolya <tollyatti25503@gmail.com>
2023-07-07 16:08:42 +1000

    added task 47, task 50 and task52

---
+++ task47/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int rows = Prompt("Введите кол-во строк m: ");
+int colums = Prompt("Введите кол-во столбцов n: ");
+double[,] array = GetArray(rows,colums);
+
+int Prompt(string message){
+    Console.WriteLine(message);
+    int number=int.Parse(Console.ReadLine());
+    return number;
+}
+
+double[,] GetArray(int m, int n){
+    double[,] result = new double[m,n];
+    for (int i = 0; i < m; i++)
+    {
+        for(int j=0; j<n;j++)
+        {
+            result[i,j]= Math.Round(new Random().NextDouble()*20 - 10,1);
+            Console.Write($"{result[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+    return result;
+}
---
+++ task50/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// [1, 7] -> на этой позиции числа в массиве нет
+
+int rows = Prompt("Введите индекс строки m: ");
+int colums = Prompt("Введите индекс столбца n: ");
+int[,] result = new int[6,7];
+array(result);
+if(rows < result.GetLength(0) && colums <result.GetLength(1)) Console.WriteLine($"{result[rows,colums]} - элемент находящийся в заданной позиции");
+else  Console.WriteLine($"[{rows};{colums}] -> Такого числа в массиве нет");
+int Prompt(string message){
+    Console.WriteLine(message);
+    int number=int.Parse(Console.ReadLine());
+    return number;
+}
+
+void array(int[,] array){
+    
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j=0; j<array.GetLength(1);j++)
+        {
+            result[i,j]= new Random().Next(0,10);
+            Console.Write($"{result[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
