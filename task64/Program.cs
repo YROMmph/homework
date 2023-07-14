f66845f034c06cdb049096eeed7d54e5a51e7861
Tolya <tollyatti25503@gmail.com>
2023-07-14 13:29:02 +1000

    added task64, task66 and task 68

---
+++ task64/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.WriteLine("Введдите N = ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"{Range(N)}");
+
+string Range(int N)
+{
+    if(N==1) return "1";
+    return N + ", " + Range(N-1);
+}
+
+
+
+
+
+
+
+// int Factorial(int N)
+// {
+//     if(N==1) return 1;
+//     else return N * Factorial(N-1);
+// }
+// Console.WriteLine(Factorial(3));
---
+++ task66/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+class Program
+{
+    static int SumOfNaturals(int m, int n)
+    {
+        if (m == n)
+        {
+            return m;
+        }
+        else
+        {
+            return m + SumOfNaturals(m+1, n);
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        Console.Write("Введите значение M: ");
+        int m = int.Parse(Console.ReadLine());
+
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (m > n)
+        {
+            Console.WriteLine("Значение M должно быть меньше или равно значению N");
+        }
+        else
+        {
+            Console.WriteLine("Сумма натуральных чисел в промежутке от {0} до {1} равна {2}", m, n, SumOfNaturals(m, n));
+        }
+    }
+}
+
